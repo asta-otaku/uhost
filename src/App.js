5cd3a75f346ef6af785969144771e6d5f94468f8
asta-otaku <88886556+asta-otaku@users.noreply.github.com>
2022-09-04 14:18:04 +0100

    --

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-
-import './App.css';
-import Header from './components/Header Component/Header';
-import { Routes, Route } from 'react-router-dom'
-import Homepage from './components/Homepage/Homepage';
-import Customers from './components/Customer Component/Customers';
-import Packages from './components/Packages Component/Packages';
-import Hosting from './components/Hosting Component/Hosting';
-import InvalidPage from './InvalidPage';
+import "./App.css";
+import Header from "./components/Header Component/Header";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./components/Homepage/Homepage";
+import Customers from "./components/Customer Component/Customers";
+import Packages from "./components/Packages Component/Packages";
+import Hosting from "./components/Hosting Component/Hosting";
+import InvalidPage from "./utils/InvalidPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/packages' element={<Packages />} />
-        <Route path='/customers' element={<Customers />} />
-        <Route path='/start-hosting' element={<Hosting />} />
-        <Route path='*' element={<InvalidPage/>} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/packages" element={<Packages />} />
+        <Route path="/customers" element={<Customers />} />
+        <Route path="/start-hosting" element={<Hosting />} />
+        <Route path="*" element={<InvalidPage />} />
       </Routes>
     </div>
   );
